0851600faa0cdfc8d426e2a6eda3d464a1977fd3
Sebastian Hurtado <sebassdc@gmail.com>
2018-03-02 11:12:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// var ball = document.querySelector('.ball');
+// var garden = document.querySelector('.garden');
+// var output = document.querySelector('.output');
+
+// var maxX = garden.clientWidth - ball.clientWidth;
+// var maxY = garden.clientHeight - ball.clientHeight;
+
+// function handleOrientation(event) {
+//   var x = event.beta; // In degree in the range [-180,180]
+//   var y = event.gamma; // In degree in the range [-90,90]
+
+//   output.innerHTML = "beta : " + x + "\n";
+//   output.innerHTML += "gamma: " + y + "\n";
+
+//   // Because we don't want to have the device upside down
+//   // We constrain the x value to the range [-90,90]
+//   if (x > 90) {
+//     x = 90
+//   };
+//   if (x < -90) {
+//     x = -90
+//   };
+
+//   // To make computation easier we shift the range of 
+//   // x and y to [0,180]
+//   x += 90;
+//   y += 90;
+
+//   // 10 is half the size of the ball
+//   // It center the positioning point to the center of the ball
+//   ball.style.top = (maxX * x / 180 - 10) + "px";
+//   ball.style.left = (maxY * y / 180 - 10) + "px";
+// }
+
+// window.addEventListener('deviceorientation', handleOrientation);
+
+var flat = document.getElementById("flat"),
+  flatStyle = flat.style,
+  _transform = "WebkitTransform" in flatStyle ? "WebkitTransform" :
+  "MozTransform" in flatStyle ? "MozTransform" :
+  "msTransform" in flatStyle ? "msTransform" : false;
+
+_transform && window.addEventListener("deviceorientation", function (e) {
+  flatStyle[_transform] = "perspective(500px) rotateY(" + (-e.gamma) + "deg) rotateX(" + e.beta + "deg) rotateZ(" + -(e.alpha - 180) + "deg)";
+});
